a7f272f9f81fc30c3ecb61919d05d6e4e7e588fc
Alex Lapins <alex@lapins.com>
2026-04-11 23:49:08 -0400

    Add launchers for common site configurations in chrome

---
+++ dot_local/bin/executable_chrome-books.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+google-chrome --new-window `jq '.roots.other.children[] | select(.name == "Books Downloads").children[].url' ~/.config/google-chrome/Default/Bookmarks | tr '\n' ' ' | sed s/\"//g`
